e4921376577f9e434817da84cd313b78f0f9af38
Gabriela P <gabriela.poryazova@gmail.com>
2023-08-26 21:39:34 +0300

    Configure next-auth auth. API

---
+++ utils/db.js
@@ -0,0 +1,24 @@
+import mongoose from 'mongoose'
+
+let isConnected = false;
+
+export const connectToDB = async () => {
+    mongoose.set('strictQuery', true)
+
+    if(isConnected){
+        console.log('connected');
+        return
+    }
+    try {
+        await mongoose.connect(process.env.MONGODB_URI, {
+            dbName: 'share-prompt',
+            useNewUrlParser: true,
+            useUnifiedTopology:true
+        }
+            )
+            isConnected = true
+
+    } catch(error){
+        console.log(user);
+    }
+}
